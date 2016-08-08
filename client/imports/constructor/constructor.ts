24b7206d361f663f5f085a9f4bba058c3d9748a7
Apetrich <apetrichsheva@gmail.com>
2016-08-08 18:09:34 +0600

    1

---
+++ client/imports/constructor/constructor.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+
+import template from './constructor.html';
+
+@Component({
+  selector: 'constructor',
+  template,
+  directives: [ROUTER_DIRECTIVES]
+})
+
+export class Item {
+  constructor() {}
+}
