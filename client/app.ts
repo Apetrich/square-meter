77333f159f3806ad34aea40f8bf40017e61215f7
Petrichsheva Anastassiya <apetrichsheva@gmail.com>
2016-08-16 23:44:32 +0600

    1

--- client/app.ts
+++ client/app.ts
@@ -3,29 +3,28 @@ import { Component, provide } from '@angular/core';
 import { bootstrap } from 'angular2-meteor-auto-bootstrap';
 import { provideRouter, RouterConfig, ROUTER_DIRECTIVES } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
-import { BreadcrumbComponent, BreadcrumbService } from 'ng2-breadcrumb/ng2-breadcrumb';
 import { Home } from './imports/home/home.ts';
 import { Catalog } from './imports/catalog/catalog.ts';
 import { Item } from './imports/item/item.ts';
 import { Constructor } from './imports/constructor/constructor.ts';
+import { Contact } from './imports/contact/contact.ts';
 
 import template from './app.html';
 
 @Component({
    selector: 'app',
    template,
-   directives: [ROUTER_DIRECTIVES, BreadcrumbComponent]
+   directives: [ROUTER_DIRECTIVES]
  })
 
 class SquareMeter {
-  constructor (private breadcrumbService: BreadcrumbService) {
-
-
+  constructor () {
   }
 }
 
 const routes: RouterConfig = [
   { path: '', component: Home },
+  { path: 'contact', component: Contact },
   { path: 'catalog', component: Catalog },
   { path: 'item/:itemId', component: Item },
   { path: 'constructor/:itemId', component: Constructor }
@@ -35,4 +34,4 @@ const APP_ROUTER_PROVIDERS = [
   provideRouter(routes)
 ];
 
-bootstrap(SquareMeter, [APP_ROUTER_PROVIDERS, provide(APP_BASE_HREF, { useValue: '/' }), BreadcrumbService]);
+bootstrap(SquareMeter, [APP_ROUTER_PROVIDERS, provide(APP_BASE_HREF, { useValue: '/' })]);
